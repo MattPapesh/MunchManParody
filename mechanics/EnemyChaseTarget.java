d8c86014cdcabb62c28d53ac8cd48c321c3afac9
Matthew Papesh <55628349+MattPapesh@users.noreply.github.com>
2023-01-06 16:48:10 -0600

    working enemies

--- mechanics/EnemyChaseTarget.java
+++ mechanics/EnemyChaseTarget.java
@@ -1,21 +1,25 @@
 package mechanics;
 
 import java.util.LinkedList;
+import java.util.NoSuchElementException;
 
 import components.Enemy;
 import components.MunchMan;
 import components.Stage;
 import fundamentals.Coordinates;
+import fundamentals.mechanic.Delay;
 import fundamentals.mechanic.MechanicBase;
+import fundamentals.mechanic.MechanicScheduler;
 
 public class EnemyChaseTarget extends MechanicBase
 {
+    private Delay update_delay = new Delay(10000, null);
     private EntityMovement enemy_movement = null;
     private Stage stage = null; 
     private MunchMan munch_man = null;
     private Enemy enemy = null; 
+    private EnemyPredeterminedRoute enemy_route = null; 
     private int[][] stage_data = null; 
-    private int max_recursion_level = 100; 
 
     public EnemyChaseTarget(EntityMovement enemy_movement, Stage stage, MunchMan munch_man, Enemy enemy)
     {   
@@ -37,50 +41,46 @@ public class EnemyChaseTarget extends MechanicBase
         return Math.abs(primary.getDegrees() - secondary.getDegrees()) == 180; 
     }
 
-    private LinkedList<Coordinates> getShortestRoute(LinkedList<LinkedList<Coordinates>> routes)
-    {
-        LinkedList<Coordinates> route = routes.getFirst();
-        for(int i = 0; i < routes.size(); i++)
-        {
-            if(routes.get(i).size() < route.size())
-            {
-                route = routes.get(i);
-            }
-        }
-
-        return route;
-    }
-
     private EnemyPredeterminedRoute getPredeterminedRoute(LinkedList<Coordinates> route)
     {
         EnemyPredeterminedRoute predetermined_route = new EnemyPredeterminedRoute(enemy_movement, enemy);
+        int paths_delta_stage_x = 0;
+        int paths_delta_stage_y = 0;
+
+        int prev_delta_stage_x = 0;
+        int prev_delta_stage_y = 0;
+        int current_delta_stage_x = 0;
+        int current_delta_stage_y = 0;
+
         for(int i = 1; i < route.size(); i++)
         {
-            predetermined_route.addRelativePath(route.get(i).getX() - route.get(i - 1).getX(), 
-            route.get(i).getY() - route.get(i - 1).getY());
+            prev_delta_stage_x = current_delta_stage_x;
+            prev_delta_stage_y = current_delta_stage_y;
+            current_delta_stage_x = route.get(i).getX() - route.get(i - 1).getX();
+            current_delta_stage_y = route.get(i).getY() - route.get(i - 1).getY();
+
+            if(prev_delta_stage_x == 0 && current_delta_stage_x != 0
+            && prev_delta_stage_y != 0 && current_delta_stage_y == 0)
+            {
+                
+            }
+
+            predetermined_route.addRelativePath(, 
+            );
         }
 
         predetermined_route.compileRelativePaths();
         return predetermined_route;
     }
-    
-    private LinkedList<LinkedList<Coordinates>> getPathVariant(LinkedList<Coordinates> base_path, LinkedList<Coordinates> path, int target_stage_x, int target_stage_y,
-    int recursion_level)
+
+    private LinkedList<Coordinates> getBranchRouteVariant(LinkedList<Coordinates> base_route, LinkedList<Coordinates> route, int target_stage_x, int target_stage_y)
     {
         try
         {
-            if(!isCoordsEqual(path.getLast(), base_path.getLast()) && !isCoordsEqual(path.getLast(), new Coordinates(target_stage_x, target_stage_y, 0))
-            && !isOpposingCoordDirections(path.getLast(), base_path.getLast()) 
-            && stage_data[path.getLast().getY()][path.getLast().getX()] == 1 && recursion_level < max_recursion_level)
+            if(!isCoordsEqual(route.getLast(), base_route.getLast()) && !isOpposingCoordDirections(route.getLast(), base_route.getLast()) 
+            && stage_data[route.getLast().getY()][route.getLast().getX()] == 1)
             {
-                return getRoutes(path, target_stage_x, target_stage_y, recursion_level);
-            }
-            else if(!isCoordsEqual(path.getLast(), base_path.getLast()) && isCoordsEqual(path.getLast(), new Coordinates(target_stage_x, target_stage_y, 0))
-            && stage_data[path.getLast().getY()][path.getLast().getX()] == 1 && recursion_level < max_recursion_level)
-            {
-                LinkedList<LinkedList<Coordinates>> listed_path = new LinkedList<LinkedList<Coordinates>>();
-                listed_path.addLast(path);
-                return listed_path;
+                return route;
             }
         }
         catch(ArrayIndexOutOfBoundsException e) {}
@@ -88,51 +88,71 @@ public class EnemyChaseTarget extends MechanicBase
         return null;
     }
 
-    public LinkedList<LinkedList<Coordinates>> getRoutes(LinkedList<Coordinates> base_route, int target_stage_x, int target_stage_y, int prev_recursion_level)
+    public LinkedList<LinkedList<Coordinates>> getBranchRoutes(LinkedList<Coordinates> base_route, int target_stage_x, int target_stage_y, int prev_recursion_level)
     {
+        // Branch routes:
         LinkedList<LinkedList<Coordinates>> routes = new LinkedList<LinkedList<Coordinates>>();
-        LinkedList<Coordinates> left_paths = new LinkedList<Coordinates>(base_route);
-        LinkedList<Coordinates> right_paths = new LinkedList<Coordinates>(base_route);
-        LinkedList<Coordinates> up_paths = new LinkedList<Coordinates>(base_route);
-        LinkedList<Coordinates> down_paths = new LinkedList<Coordinates>(base_route);
+        LinkedList<Coordinates> left_route = new LinkedList<Coordinates>(base_route);
+        LinkedList<Coordinates> right_route = new LinkedList<Coordinates>(base_route);
+        LinkedList<Coordinates> up_route = new LinkedList<Coordinates>(base_route);
+        LinkedList<Coordinates> down_route = new LinkedList<Coordinates>(base_route);
         
-        left_paths.addLast(new Coordinates(base_route.getLast().getX() - 1, base_route.getLast().getY(), 180));
-        right_paths.addLast(new Coordinates(base_route.getLast().getX() + 1, base_route.getLast().getY(), 0));
-        up_paths.addLast(new Coordinates(base_route.getLast().getX(), base_route.getLast().getY() - 1, 90));
-        down_paths.addLast(new Coordinates(base_route.getLast().getX(), base_route.getLast().getY() + 1, 270));
+        left_route.addLast(new Coordinates(base_route.getLast().getX() - 1, base_route.getLast().getY(), 180));
+        right_route.addLast(new Coordinates(base_route.getLast().getX() + 1, base_route.getLast().getY(), 0));
+        up_route.addLast(new Coordinates(base_route.getLast().getX(), base_route.getLast().getY() - 1, 90));
+        down_route.addLast(new Coordinates(base_route.getLast().getX(), base_route.getLast().getY() + 1, 270));
 
-        try
-        {
-            if(prev_recursion_level + 1 < max_recursion_level) 
-            {    
-                LinkedList<LinkedList<Coordinates>> left_routes = getPathVariant(base_route, left_paths, target_stage_x, target_stage_y, prev_recursion_level + 1);
-                LinkedList<LinkedList<Coordinates>> right_routes = getPathVariant(base_route, right_paths, target_stage_x, target_stage_y, prev_recursion_level + 1);
-                LinkedList<LinkedList<Coordinates>> up_routes = getPathVariant(base_route, up_paths, target_stage_x, target_stage_y, prev_recursion_level + 1);
-                LinkedList<LinkedList<Coordinates>> down_routes = getPathVariant(base_route, down_paths, target_stage_x, target_stage_y, prev_recursion_level + 1);
+        left_route = getBranchRouteVariant(base_route, left_route, target_stage_x, target_stage_y);
+        right_route = getBranchRouteVariant(base_route, right_route, target_stage_x, target_stage_y);
+        up_route = getBranchRouteVariant(base_route, up_route, target_stage_x, target_stage_y);
+        down_route = getBranchRouteVariant(base_route, down_route, target_stage_x, target_stage_y);
 
-                if(left_routes != null) 
-                    routes.addAll(left_routes);
-                if(right_routes != null) 
-                    routes.addAll(right_routes);
-                if(up_routes != null) 
-                    routes.addAll(up_routes);
-                if(down_routes != null) 
-                    routes.addAll(down_routes);
-            }    
-        }
-        catch(StackOverflowError e) {}
+        if(left_route != null)
+            routes.addLast(left_route);
+        if(right_route != null)
+            routes.addLast(right_route);
+        if(up_route != null)
+            routes.addLast(up_route);
+        if(down_route != null)
+            routes.addLast(down_route);
 
         return routes;
+    }    
+
+    private LinkedList<Coordinates> getRouteFound(LinkedList<LinkedList<Coordinates>> routes, int target_stage_x, int target_stage_y)
+    {
+        for(int i = 0; i < routes.size(); i++)
+        {
+            if(routes.get(i).getLast().getX() == target_stage_x && routes.get(i).getLast().getY() == target_stage_y)
+            {
+                return routes.get(i);
+            }
+        }
+
+        return null; 
     }
 
+    // when building new route with routes: a route is already logged if the next route found is found in a logged route
     public EnemyPredeterminedRoute getRoute(int target_stage_x, int target_stage_y)
     {
-        LinkedList<Coordinates> initial_path = new LinkedList<Coordinates>();
-        initial_path.addLast(new Coordinates(enemy.getStageCoords().getX(), enemy.getStageCoords().getY(), 0));
-        LinkedList<LinkedList<Coordinates>> routes = getRoutes(initial_path, target_stage_x, target_stage_y, 0);
-        LinkedList<Coordinates> shortest_route = getShortestRoute(routes);
+        LinkedList<Coordinates> initial_route = new LinkedList<Coordinates>();
+        LinkedList<LinkedList<Coordinates>> logged_routes = new LinkedList<LinkedList<Coordinates>>();
+        initial_route.addLast(new Coordinates(enemy.getStageCoords().getX(), enemy.getStageCoords().getY(), 0));
+        logged_routes.addLast(initial_route);
 
-        return getPredeterminedRoute(shortest_route); 
+        while(getRouteFound(logged_routes, target_stage_x, target_stage_y) == null)
+        {
+            LinkedList<LinkedList<Coordinates>> updated_logged_routes = new LinkedList<LinkedList<Coordinates>>();
+            for(int i = 0; i < logged_routes.size(); i++)
+            {
+                updated_logged_routes.addAll(getBranchRoutes(logged_routes.get(i), target_stage_x, target_stage_y, 0));
+            }
+
+            logged_routes.clear();
+            logged_routes.addAll(updated_logged_routes);
+        }
+
+        return getPredeterminedRoute(getRouteFound(logged_routes, target_stage_x, target_stage_y));
     }
 
     @Override
@@ -147,7 +167,19 @@ public class EnemyChaseTarget extends MechanicBase
     @Override
     public void execute()
     {
-
+        try
+        {
+            if(!enemy_route.isScheduled())
+            {
+                enemy_route = getRoute(munch_man.getStageCoords().getX(), munch_man.getStageCoords().getY());
+                enemy_route.schedule();
+            }
+        }
+        catch(NullPointerException e) 
+        {
+            enemy_route = getRoute(munch_man.getStageCoords().getX(), munch_man.getStageCoords().getY());
+            enemy_route.schedule();
+        }
     }
 
     @Override
